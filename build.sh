368e5e6140510632a07b78b1dcfa9364255bceb8
Chris Pressey <cpressey@catseye.tc>
2019-08-21 14:03:59 +0100

    Modernize build script and wrappers.

--- build.sh
+++ build.sh
@@ -1,34 +1,12 @@
 #!/bin/sh
 
-if [ x`which ghc` = x -a x`which runhugs` = x ]; then
-    echo "Neither ghc nor runhugs found on search path."
-    exit 1
-fi
-
-mkdir -p bin
-
-if [ x`which ghc` = x -o ! x$USE_HUGS = x ]; then
-    # create scripts to run with Hugs
-    cat >bin/robinri <<'EOF'
-#!/bin/sh
-THIS=`realpath $0`
-DIR=`dirname $THIS`/../src
-cp $DIR/mains/robinri/Main.lhs $DIR/Main.lhs
-runhugs $DIR/Main.lhs $*
-EOF
-    chmod 755 bin/robinri
-    cat >bin/whitecap <<'EOF'
-#!/bin/sh
-THIS=`realpath $0`
-DIR=`dirname $THIS`/../src
-cp $DIR/mains/whitecap/Main.lhs $DIR/Main.lhs
-runhugs $DIR/Main.lhs $*
-EOF
-    chmod 755 bin/whitecap
-else
-    rm -f src/Main.lhs
-    ghc -isrc --make src/mains/robinri/Main.lhs -o bin/robinri || exit 1
-    ghc -isrc --make src/mains/whitecap/Main.lhs -o bin/whitecap || exit 1
-fi
+for PROG in robinri whitecap; do
+    if command -v ghc >/dev/null 2>&1; then
+        echo "building $PROG.exe with ghc"
+        ghc -isrc --make src/mains/$PROG/Main.lhs -o bin/$PROG.exe || exit 1
+    else
+        echo "ghc not found, not building $PROG.exe"
+    fi
+done
 
 ./build-packages.sh
--- clean.sh
+++ clean.sh
@@ -2,4 +2,5 @@
 
 find . -name "*.o"  -exec rm {} \;
 find . -name "*.hi" -exec rm {} \;
+find . -name "*.exe" -exec rm {} \;
 rm -rf pkg/*.robin
