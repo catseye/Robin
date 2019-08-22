50ded2b53c6bdd46690c6f094f005be38b9bcf8a
Chris Pressey <cpressey@catseye.tc>
2019-08-22 16:33:27 +0100

    Un-lhs-ify the remaining modules.

--- TODO.md
+++ TODO.md
@@ -3,6 +3,8 @@ TODO
 
 Test that circular definitions are not allowed.
 
+Move all the tests to the bottom (or the top?) of each defining file.
+
 Long-term
 ---------
 
@@ -10,12 +12,12 @@ Environments as abstract maps, alist->env, env->alist
 
 Values of "opaque" type, to support that.
 
+Top-level: `assert`, `requires` (sugar for a form of `assert`).
+
 Maybe
 -----
 
 Evaluate a Robin expression and display it? REPL?
-
-Un-literate all the .lhs files?
 
 Bring together the docs into a single large spec?
 
---
+++ src/Language/Robin/Env.hs
@@ -0,0 +1,25 @@
+module Language.Robin.Env where
+
+import Language.Robin.Expr
+
+--
+-- An environment is an alist which associates symbols with
+-- values (arbitrary S-expressions).
+--
+
+empty = List []
+
+insert s@(Symbol _) value env =
+    append (List [List [s, value]]) env
+
+find s@(Symbol _) (List []) = Nothing
+find s@(Symbol _) (List (List [first, value]:rest))
+    | s == first   = Just value
+    | otherwise    = find s (List rest)
+
+fromList [] =
+    List []
+fromList ((id, val):xs) =
+    append (List [List [(Symbol id), val]]) (fromList xs)
+
+mergeEnvs (List a) (List b) = List (a ++ b)
