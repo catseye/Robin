5dd6a3bf82113e582efb37ea71a56419e32d60b9
Chris Pressey <cpressey@catseye.tc>
2019-09-10 21:47:08 +0100

    Factor common code out into CmdLine module.

--- src/HugsMain.hs
+++ src/HugsMain.hs
@@ -1,16 +1,13 @@
 module Main where
 
-import System.IO
-
 import System.Environment
 import System.Exit
 
-import Language.Robin.Expr
+import Language.Robin.CmdLine
+
 import Language.Robin.Env (mergeEnvs)
-import Language.Robin.Parser (parseToplevel, parseExpr)
 import Language.Robin.Intrinsics (robinIntrinsics)
 import Language.Robin.Builtins (robinBuiltins)
-import qualified Language.Robin.TopLevel as TopLevel
 
 import Language.Robin.EventLoop (eventLoop)
 import Language.Robin.Facilities (handler, waiter)
@@ -33,44 +30,3 @@ main = do
             let waitForEvents = waiter lineTerminal
             eventLoop showEvents handlers waitForEvents reactors
             exitWith ExitSuccess
-
-
-abortWith msg = do
-    hPutStrLn stderr msg
-    exitWith $ ExitFailure 1
-
-
-processFlags ("--no-builtins":rest) env showEvents = processFlags rest robinIntrinsics showEvents
-processFlags ("--show-events":rest) env showEvents = processFlags rest env True
-processFlags args env showEvents = (args, env, showEvents)
-
-
-processArgs args env = processArgs' args env [] [] where
-    processArgs' [] env reactors results = return (env, reactors, results)
-    processArgs' ("eval":filename:rest) env reactors results = do
-        exprText <- readFile filename
-        case parseExpr exprText of
-            Right expr -> do
-                let topExprs = [List [Symbol "display", expr]]
-                (env', reactors', results') <- return $ TopLevel.collect topExprs env reactors results
-                processArgs' rest env' reactors' results'
-            Left problem -> do
-                hPutStr stderr (show problem)
-                exitWith $ ExitFailure 1
-    processArgs' (filename:rest) env reactors results = do
-        program <- readFile filename
-        case parseToplevel program of
-            Right topExprs -> do
-                (env', reactors', results') <- return $ TopLevel.collect topExprs env reactors results
-                processArgs' rest env' reactors' results'
-            Left problem -> do
-                hPutStr stderr (show problem)
-                exitWith $ ExitFailure 1
-
-
-writeResults [] = return ()
-writeResults ((Right result):results) = do
-    putStrLn $ show result
-    writeResults results
-writeResults ((Left expr):results) =
-    error $ "uncaught exception: " ++ show expr
