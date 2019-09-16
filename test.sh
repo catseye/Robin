07ba13ac83fb6353f74fb677251ab1f82ce04605
Chris Pressey <cpressey@catseye.tc>
2019-09-17 00:56:04 +0100

    Small improvements to test script.

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-./build.sh || exit 1
+if [ "${NOBUILD}x" = "x" ]; then
+  ./build.sh || exit 1
+fi
 
 if [ "${APPLIANCES}x" = "x" ]; then
   APPLIANCES="appliances/robin.md appliances/robin-no-builtins.md"
@@ -9,7 +11,9 @@ fi
 echo "Running tests on core semantics..."
 falderal -b $APPLIANCES doc/Robin.md || exit 1
 
-for PACKAGE in intrinsics small boolean arith list env misc; do
+PACKAGES="intrinsics small boolean arith list env misc"
+
+for PACKAGE in $PACKAGES; do
     echo "Running tests on '$PACKAGE' package..."
     falderal -b $APPLIANCES pkg/$PACKAGE.robin || exit 1
 done
