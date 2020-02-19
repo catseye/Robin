52edfd19fd8220a4ff080bf52022877348a00549
Chris Pressey <cpressey@catseye.tc>
2020-02-19 09:27:12 +0000

    Finish renaming builtins.

--- src/Language/Robin/Builtins.hs
+++ src/Language/Robin/Builtins.hs
@@ -1,7 +1,7 @@
 module Language.Robin.Builtins where
 
 import Prelude (
-    ($), (>), (>=), (<), (<=), (+), (*), div, mod, abs, map, reverse, Bool(True, False)
+    ($), (>), (>=), (<), (<=), (+), (*), div, mod, map, reverse, Bool(True, False)
   )
 import qualified Prelude as P
 
@@ -140,26 +140,26 @@ ltP = evalTwoNumbers (\x y cc -> cc $ Boolean (x < y))
 lteP :: Evaluable
 lteP = evalTwoNumbers (\x y cc -> cc $ Boolean (x <= y))
 
-robinAbs :: Evaluable
-robinAbs env (List [expr]) cc =
-    eval env expr (\x -> assertNumber env x (\(Number xv) -> cc (Number $ abs xv)))
-robinAbs env other cc = errMsg "illegal-arguments" other
+abs :: Evaluable
+abs env (List [expr]) cc =
+    eval env expr (\x -> assertNumber env x (\(Number xv) -> cc (Number $ P.abs xv)))
+abs env other cc = errMsg "illegal-arguments" other
 
-robinAdd :: Evaluable
-robinAdd = evalTwoNumbers (\x y cc -> cc $ Number (x + y))
+add :: Evaluable
+add = evalTwoNumbers (\x y cc -> cc $ Number (x + y))
 
-robinMultiply :: Evaluable
-robinMultiply = evalTwoNumbers (\x y cc -> cc $ Number (x * y))
+multiply :: Evaluable
+multiply = evalTwoNumbers (\x y cc -> cc $ Number (x * y))
 
-robinDivide :: Evaluable
-robinDivide = evalTwoNumbers (\x y cc -> case y of
+divide :: Evaluable
+divide = evalTwoNumbers (\x y cc -> case y of
                                  0 -> errMsg "division-by-zero" $ Number x
                                  _ -> cc $ Number (x `div` y))
 
-robinRemainder :: Evaluable
-robinRemainder = evalTwoNumbers (\x y cc -> case y of
+remainder :: Evaluable
+remainder = evalTwoNumbers (\x y cc -> case y of
                                  0 -> errMsg "division-by-zero" $ Number x
-                                 _ -> cc $ Number (abs (x `mod` y)))
+                                 _ -> cc $ Number (P.abs (x `mod` y)))
 
 --
 -- Mapping of names to our functions, providing an evaluation environment.
@@ -182,9 +182,9 @@ robinBuiltins = fromList $ map (\(name,bif) -> (name, Intrinsic name bif))
         ("lt?",       ltP),
         ("lte?",      lteP),
 
-        ("abs",       robinAbs),
-        ("add",       robinAdd),
-        ("multiply",  robinMultiply),
-        ("divide",    robinDivide),
-        ("remainder", robinRemainder)
+        ("abs",       abs),
+        ("add",       add),
+        ("multiply",  multiply),
+        ("divide",    divide),
+        ("remainder", remainder)
       ]
