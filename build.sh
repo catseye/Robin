3436c8b3bb643c8617bcbc936c8fd708fc112be3
Chris Pressey <cpressey@catseye.tc>
2021-08-24 06:43:31 +0100

    Make JavaScript version buildable with hastec-in-Docker-container.

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ fi
 
 if command -v hastec >/dev/null 2>&1; then
     echo "building $PROG.js with hastec"
-    (cd src && hastec --make HasteMain.hs -o ../demo/$PROG.js) || exit 1
+    (cd src && hastec --make HasteMain.hs -o $PROG.js && mv $PROG.js ../demo/$PROG.js) || exit 1
 else
     echo "hastec not found, not building $PROG.js"
 fi
